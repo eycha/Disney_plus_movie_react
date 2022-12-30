271373e213f6dabc4da174b56c8623675cd07e08
ey.cha <silvlexonst@gmail.com>
2022-12-30 12:18:20 +0900

    build: structure adding

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoivedb.org/3",
+  params: {
+    api_key: "f643ad5042c57933d8eec6a7a984a73c",
+    language: "ko-KR",
+  },
+});
+
+export default instance;
---
+++ src/api/request.js
@@ -0,0 +1,12 @@
+const requests = {
+  fetchNowPlaying: "movie/now_playing",
+  fetchTrending: "/trending/all/week",
+  fetchTopRated: "/movie/top_rated",
+  fetchActionMoives: "/discover/moive?with_genres=28",
+  fetchComedyMoives: "/discover/moive?with_genres=35",
+  fetchHorrorMoives: "/discover/moive?with_genres=27",
+  fetchRomanceMoives: "/discover/moive?with_genres=10749",
+  fetchDocumentaries: "/discover/moive?with_genres=99",
+};
+
+export default requests;
