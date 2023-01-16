fbf55a8f470c15bef3ace5437d018c25abb6dde1
ey.cha <silvlexonst@gmail.com>
2023-01-16 15:42:41 +0900

    feat: banner css adding

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -25,7 +25,34 @@ const Banner = () => {
     setMovie(movieDetail);
   };
 
-  return <div>Banner</div>;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+        <p className="banner__description">{movie.overview}</p>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 };
 
 export default Banner;
