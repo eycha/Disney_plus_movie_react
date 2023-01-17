42f1790b30022215625acc6fde0448a3be606244
차은엽 <eycha@cha-eun-yeob-ui-MacBookAir.local>
2023-01-17 23:03:22 +0900

    feat: youtube embeded

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axiosInstance from "../api/axios";
 import requests from "../api/request";
 import "./Banner.css";
+import styled from "styled-components";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -25,8 +27,8 @@ const Banner = () => {
       ].id;
 
     //특정 영화의 더 상세한 정보를 가져오괴 (비디오 정보도 포함)
-    const {data: movieDetail} = await axiosInstance.get(`movie/${movieId}`, {
-      params: {append_to_response: "videos"},
+    const { data: movieDetail } = await axiosInstance.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
     });
     setMovie(movieDetail);
   };
@@ -35,30 +37,82 @@ const Banner = () => {
     return str?.length > n ? str.substring(0, n) + "..." : str;
   };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div style={{fontSize: 10}} className="banner__contents">
-        <h1 className="banner__title">
-          {movie.title || movie.name || movie.original_name}
-        </h1>
+  if (isClicked) {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  } else {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div style={{ fontSize: 10 }} className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
 
-        <div className="banner__buttons">
-          {movie?.videos?.results[0]?.key && (
-            <button className="banner__button play">Play</button>
-          )}
+          <div className="banner__buttons">
+            {movie?.videos?.results[0]?.key && (
+              <button
+                className="banner__button play"
+                onClick={() => setIsClicked(true)}
+              >
+                Play
+              </button>
+            )}
+          </div>
+          <p className="banner__description">{truncate(movie.overview, 120)}</p>
         </div>
-        <p className="banner__description">{truncate(movie.overview, 120)}</p>
-      </div>
-      <div className="banner--fadeBottom" />
-    </header>
-  );
+        <div className="banner--fadeBottom" />
+      </header>
+    );
+  }
 };
 
 export default Banner;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
